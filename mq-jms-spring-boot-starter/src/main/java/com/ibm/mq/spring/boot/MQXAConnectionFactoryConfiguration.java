d47a01033945221cfbd22ba1143d4078fcdc2239
longstone <git@longstone.ch>
2018-05-27 19:11:41 +0200

    I'm not sure, how did this ever worked :worried:

--- mq-jms-spring-boot-starter/src/main/java/com/ibm/mq/spring/boot/MQXAConnectionFactoryConfiguration.java
+++ mq-jms-spring-boot-starter/src/main/java/com/ibm/mq/spring/boot/MQXAConnectionFactoryConfiguration.java
@@ -21,7 +21,7 @@ import javax.jms.ConnectionFactory;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.jta.XAConnectionFactoryWrapper;
+import org.springframework.boot.jms.XAConnectionFactoryWrapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
